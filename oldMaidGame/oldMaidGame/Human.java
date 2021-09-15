100c6c326350a025966d71f8df894224bd78fbd1
kensho hanano <>
2021-09-15 23:35:56 +0900

    作業を進めた

--- oldMaidGame/oldMaidGame/Human.java
+++ oldMaidGame/oldMaidGame/Human.java
@@ -1,7 +1,13 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public abstract class Human {
+    final Decision decision;
+
+    public Human(Decision decision) {
+        this.decision = decision;
+    }
     private List <Card> myCardList = new ArrayList<>();
 
     public List<Card> getMyCardList() {
@@ -12,15 +18,11 @@ public abstract class Human {
         this.myCardList.add(myCardList);
     }
 
-    public boolean equals(Object o ){
-        if(o == this)return true;
-        if(o == null)return false;
-        if(!(o instanceof Human))return false;
-        Human h = (Human) o;
-        if(!this.myCardList.equals(h.myCardList)){
-            return false;
-        }
-        return true;
+    public void draw(List<Card> myCardList , List<Card> pairCardList){
+        Random rand = new Random();
+        decision.decision(pairCardList.get(rand.nextInt(pairCardList.size())),myCardList);
     }
+
+    public abstract void firstDraw(List<Card> deckCardList , List<Card> myCardList);
 }
 
